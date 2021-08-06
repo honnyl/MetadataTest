193f57ced39c58ccc1842be6a1ddd7c320ed0a9d
honnyl <893251017@qq.com>
2021-08-06 13:58:23 +0800

    lhn option of test

---
+++ src/main/java/com/test/metadata/TestJob.java
@@ -0,0 +1,13 @@
+package com.test.metadata;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: honnylious
+ * \* Date: 2021/8/6
+ * \* Time: 下午1:24
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class TestJob {
+}
---
+++ src/main/java/com/test/metadata/metadatajob/MetaDataJob.java
@@ -0,0 +1,13 @@
+package com.test.metadata.metadatajob;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: honnylious
+ * \* Date: 2021/8/6
+ * \* Time: 下午1:35
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class MetaDataJob {
+}
